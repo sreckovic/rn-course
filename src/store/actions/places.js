242a3942e90c5541c32690733293a0343cc2b9dd
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-02-28 17:29:37 +0100

    addPlace to Firebase
    addPlace to Firebase using Fetch, name + location

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,31 @@
 import * as actionTypes from './actionTypes';
 
+/*
 export const addPlace = (placeName, location, image) => {
   return {
     type: actionTypes.ADD_PLACE,
     placeName: placeName,
     location: location,
     image: image
+  };
+};
+*/
+
+export const addPlace = (placeName, location, image) => {
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch('https://awesome-places-r-1519732224475.firebaseio.com/places.json', {
+      method: 'POST',
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
   };
 };
 
